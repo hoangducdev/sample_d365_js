4172fbf441f7f34d8f2fe2408a916f246b43191a
ducn20 <ducnh20@fpt.com>
2024-08-12 14:54:28 +0700

    first commit

---
+++ account.js
@@ -0,0 +1,15 @@
+var accountSdk = window.accountSdk || {};
+
+(function () {
+    //#region private function
+    var myPrivate = function () {
+
+    }
+    //#region 
+
+    //#region public function
+    this.Onload = function (executionContext) {
+        var formContext = executionContext.getFormContext();
+    }
+    //#endregion
+}).call(accountSdk);
